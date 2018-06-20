a3fc2d4930c8e4cbe023e627578c85faf8e5459f
Pankin <pankin.ap.1@pg.com>
2018-06-20 03:00:15 +0300

    'gfg'

--- scr.py
+++ scr.py
@@ -5,7 +5,9 @@ bot = telebot.TeleBot("605943675:AAHbUDKkHFfyhIffuhF3m_AuGA8h8mQtz2Q")
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
 
+    bot.send_message(message.chat.id, "Пошёл ты на хер,КОЗЁЛ!!!")
+    photo = open('http://risovach.ru/upload/2013/12/generator/poshel-ty-naher-kozel_36677374_orig_.jpeg', 'rb')
+    bot.send_photo(message.chat.id, photo)
 if __name__ == '__main__':
      bot.polling(none_stop=True)
